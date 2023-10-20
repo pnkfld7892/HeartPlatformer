7b2bb486daa4c5c407748683d6306888c5d2678a
Eli Schwarz <eschwarz0@gmail.com>
2023-10-20 17:34:18 -0400

    init code

---
+++ world.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class world : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+    //
+    
+    
+	public override void _Ready()
+	{
+        var polygon2d = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
+        var collisionPoly = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
+        polygon2d.Polygon = collisionPoly.Polygon;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
