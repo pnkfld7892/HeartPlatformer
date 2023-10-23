ef6e2b5ab288841af00a036a566c828aa2db9040
Eli Schwarz <eschwarz0@gmail.com>
2023-10-22 20:59:40 -0400

    after video 3

---
+++ PlayerMovementData.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+[GodotClassName("PlayerMovementData")]
+[Tool]
+public partial class PlayerMovementData : Resource
+{
+    [Export]
+    public  float Speed = 100.0f;
+    [Export]
+    public  float Acceleration = 600.0f;
+    [Export]
+    public  float Friction = 1000.0f;
+    [Export]
+    public  float JumpVelocity = -300.0f; 
+    [Export]
+    public float GravityScale = 1.0f;
+    [Export]
+    public float AirResistance = 200.0f;
+    
+}
